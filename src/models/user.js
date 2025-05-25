775925eb3204b648e089f1cf5940b353f78e3d1c
Sanjoy Saha <sanjoysaha0524@gmail.com>
2025-05-25 16:13:31 +0530

    schema update

--- src/models/user.js
+++ src/models/user.js
@@ -2,22 +2,43 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     firstName:{
-        type: String
+        type: String,
+        required: true,
+        minLength: 4,
+        maxLength: 50,
     },
     lastName:{
         type: String
     },
     emailId:{
-        type: String
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
     },
     password:{
-        type: String
+        type: String,
+        required: true,
     },
     age:{
-        type: Number
+        type: Number,
+        min: 18,
     },
     gender:{
         type: String
+    },
+    photoUrl:{
+        type: String,
+        default: "https://weimaracademy.org/wp-content/uploads/2021/08/dummy-user.png",
+    },
+    about:{
+        type: String,
+        default: "This is default about of user",
+
+    },
+    skills:{
+        type: [String],
     },
 })
 
